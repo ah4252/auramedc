31741e011687fd192f0fcc52766fb81bb4ac627a
TabiB <abendakfal07@gmail.com>
2026-05-15 04:15:03 +0100

    Delete src directory

--- src/app/courses/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getCategories } from "@/app/actions/content";
-import YearsClient from "./components/YearsClient";
-
-export default async function CoursesPage() {
-  const categories = await getCategories("YEAR");
-
-  return <YearsClient categories={JSON.parse(JSON.stringify(categories))} />;
-}
