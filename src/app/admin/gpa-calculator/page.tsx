632ade68bc5a554879bfb4bc204d0df0eba4159c
TabiB <abendakfal07@gmail.com>
2026-05-15 03:02:43 +0100

    Add files via upload

---
+++ src/app/admin/gpa-calculator/page.tsx
@@ -0,0 +1,10 @@
+import { getGPAYears } from "@/app/actions/gpaAdmin";
+import GpaAdminClient from "./GpaAdminClient";
+
+export const dynamic = "force-dynamic";
+
+export default async function GpaCalculatorAdminPage() {
+  const gpaYears = await getGPAYears();
+
+  return <GpaAdminClient initialYears={gpaYears} />;
+}
