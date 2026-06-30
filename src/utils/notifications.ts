f1e559f6e56282808e26ab97699c5970fd43e4ca
TabiB <abendakfal07@gmail.com>
2026-06-30 23:13:27 +0100

    dr

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -14,6 +14,7 @@ export async function subscribeUser(vapidPublicKey: string) {
   try {
     const subscription = await registration.pushManager.subscribe({
       userVisibleOnly: true,
+      // @ts-ignore // PushManager expects BufferSource but VAPID key is Uint8Array
       applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
     });
     
@@ -34,5 +35,9 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
   const rawData = atob(base64);
-  return Uint8Array.from([...rawData].map(ch => ch.charCodeAt(0)));
+  const output = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    output[i] = rawData.charCodeAt(i);
+  }
+  return output;
 }
